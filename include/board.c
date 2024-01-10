0abfad7475d2982f3973245496f91e3ee824923e
MarvinKatapult <ibingmarvin@gmail.com>
2024-01-10 19:31:41 +0100

    Fixed deleteing wrong board.c

--- include/board.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-/**
- * @brief Sets the Chessboard from fenstring
- * @param fen_string Fenstring
- */
-void fromString( const char * fen_string );
---
+++ src/board.c
@@ -0,0 +1,22 @@
+#include <board.h>
+
+/**
+ * @brief Initiatlizes the Board
+ * @param board Board
+ */
+void boardInit( struct Board * p_board ) {
+    for ( int y = 0; y < COUNT_BOARD_SQUARES / 9; y++ ) {
+        for ( int x = 0; x < COUNT_BOARD_SQUARES / 9; x++ ) {
+            int index = x + y * 9;
+            struct Square * square = p_board->squares + index;
+
+            // Set Color for Squares in Board
+            if ( index % 2 == 0 ) square->color = WHITE_SQUARE;
+            else square->color = BLACK_SQUARE;
+
+            // Set x and y position
+            square->x = x;
+            square->y = y;
+        }
+    }
+}
