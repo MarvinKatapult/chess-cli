9c9abc1b313ade90b25cbf940e1d559045dbeee7
MarvinKatapult <ibingmarvin@gmail.com>
2024-01-10 17:41:24 +0100

    Board init

---
+++ include/board.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/**
+ * @brief Sets the Chessboard from fenstring
+ * @param fen_string Fenstring
+ */
+void fromString( const char * fen_string );
---
+++ src/board.c
@@ -0,0 +1,8 @@
+
+/**
+ * @brief Sets the Chessboard from fenstring
+ * @param fen_string Fenstring
+ */
+void fromString( const char * fen_string ) {
+    printf( "Fenstring: %s\n", fen_string );
+}
