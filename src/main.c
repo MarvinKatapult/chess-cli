937c61fd85968e248cad614a69170b60e3a77166
MarvinKatapult <ibingmarvin@gmail.com>
2024-01-11 19:29:19 +0100

    Fenstring fix

--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
 
 #include <stdio.h>
 #include <board.h>
-#include "tdef.h"
-#include "consoleio.h"
+#include <tdef.h>
+#include <consoleio.h>
 
 int main( int argc, string_array argv ) {
     
+    // TODO Pass fenstring as Argument with --fen or -f
+    
     char * board_string;
     // If no starting postition is passed, use default starting position
-    if ( argc < 2 ) board_string = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+    if ( argc < 2 ) board_string = STARTING_FEN_STRING;
     else board_string = argv[1];
 
     // Create Board
