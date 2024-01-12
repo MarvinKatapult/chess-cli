ae7b6ce3b1270b0f9d7a950ba3630cbb2eb11c1a
SWiRaaki <siwiraki@gmail.com>
2024-01-12 19:13:02 +0100

    Added c_scan and fixed Doxyfile

--- src/consoleio.c
+++ src/consoleio.c
@@ -1,6 +1,10 @@
 #include "consoleio.h"
 
+#include <stdlib.h>
+
+/// @private
 #define ANSI_CONTROL_CLEAR "\033[2J"
+/// @private
 #define ANSI_CONTROL_CPOS "\033[%d;%dH"
 
 /**
@@ -29,9 +33,10 @@ void c_clear( void ) {
  */
 void c_print( cstring p_format, ... ) {
     va_list args;
-    va_start(args, p_format);
-    vfprintf( stdout, p_format, args);
-    va_end(args);
+    
+    va_start( args, p_format );
+    vfprintf( stdout, p_format, args );
+    va_end( args );
 }
 
 /**
@@ -42,9 +47,10 @@ void c_print( cstring p_format, ... ) {
  */
 void c_print_err( cstring p_format, ... ) {
     va_list args;
-    va_start(args, p_format);
-    vfprintf( stdout, p_format, args);
-    va_end(args);
+    
+    va_start( args, p_format );
+    vfprintf( stdout, p_format, args );
+    va_end( args );
 }
 
 /**
@@ -56,7 +62,43 @@ void c_print_err( cstring p_format, ... ) {
  */
 void c_fprint( FILE * p_file, cstring p_format, ... ) {
     va_list args;
-    va_start(args, p_format);
-    vfprintf( p_file, p_format, args);
-    va_end(args);
+    
+    va_start( args, p_format );
+    vfprintf( p_file, p_format, args );
+    va_end( args );
+}
+
+/**
+ * @brief Reads data from stdin and stores them according to the parameter format into the locations pointed 
+ * 
+ * @param format  C string that contains a sequence of characters that control how characters extracted from the stream are treated.
+ * @param ... Additional arguments to store the imput
+ * @return * int On Success, the function returns the number of items of the argument list successfully filled.
+ */
+int c_scan( cstring p_format, ... ) {
+    int result = 0;
+    va_list args;
+
+    va_start( args, p_format );
+    result = vscanf( p_format, args );
+    va_end( args );
+
+    return result;
+}
+
+/**
+ * @brief Reads data from stdin and returns the input as a const string with max size of IN_STRING_SIZE
+ * 
+ * @return cstring C string read from stdin. Needs to be freed manually
+ */
+cstring c_scan_string( void ) {
+    return c_scan_string_size( IN_STRING_SIZE );
+}
+
+cstring c_scan_string_size( uint32 p_intput_size ) {
+    string in = (string)malloc( sizeof(char) * p_intput_size );
+    int result = c_scan( "%s", in );
+    if ( result != 0)
+        return in;
+    return NULL;
 }
